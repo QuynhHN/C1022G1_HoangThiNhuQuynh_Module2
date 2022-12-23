a6de9561df8dcfbb96728be75580f4cad121dbb1
HoangThiNhuQuynh <hoangnhuquynh.116@gmail.com>
2022-12-23 11:03:13 +0700

    up

--- Module_2/src/ss16/io_text_file/bai_tap/copy_file_text/CountCharCoppyFile.java
+++ Module_2/src/ss16/io_text_file/bai_tap/copy_file_text/CountCharCoppyFile.java
@@ -2,14 +2,18 @@ package ss16.io_text_file.bai_tap.copy_file_text;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
 
 public class CountCharCoppyFile {
     public static final String SOURCE = "C:\\Users\\DELL\\Desktop\\CODEGYM\\module_2\\Module_2\\src\\ss16\\io_text_file\\bai_tap\\copy_file_text\\sourcefile.csv";
     public static final String TARGET = "C:\\Users\\DELL\\Desktop\\CODEGYM\\module_2\\Module_2\\src\\ss16\\io_text_file\\bai_tap\\copy_file_text\\targetfile.csv";
 
     public static void main(String[] args) throws IOException {
+        System.out.println("Nhập vào đường dẫn file");
+        Scanner sc = new Scanner(System.in);
+        String path = sc.nextLine();
         ReadAndWriteFile readAndWriteFile = new ReadAndWriteFile();
-        List<String> list = ReadAndWriteFile.readFileText(SOURCE);
+        List<String> list = ReadAndWriteFile.readFileText(path);
         int count = countChar(list);
         readAndWriteFile.writeFile(TARGET, count, list);
 
